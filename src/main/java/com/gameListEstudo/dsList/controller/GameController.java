dac660e1dd8a4371ed869b97cf1103cc2338a4f2
leticiafdepaula <leticiaferreira150998@gmail.com>
2024-11-15 22:13:48 -0300

    feat: pronto para implantar

--- src/main/java/com/gameListEstudo/dsList/controller/GameController.java
+++ src/main/java/com/gameListEstudo/dsList/controller/GameController.java
@@ -2,15 +2,12 @@ package com.gameListEstudo.dsList.controller;
 
 import com.gameListEstudo.dsList.dto.GameDTO;
 import com.gameListEstudo.dsList.dto.GameMinDTO;
-import com.gameListEstudo.dsList.entities.Game;
 import com.gameListEstudo.dsList.service.GamesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
